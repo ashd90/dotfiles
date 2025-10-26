9bea6c831907b917680c8f2cbce59357b0a86677
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2025-10-26 11:15:43 +0530

    added script

---
+++ post-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yay -S ttf-firacode-nerd --noconfirm
+
+# Installing Starship Prompt
+#
+curl -sS https://starship.rs/install.sh | sh
+
+echo eval "$(starship init bash)" >> ~/.bashrc
+
+cp alaritty.toml ~/.config/alacritty/
+
+sudo pacman - < S packages.txt
